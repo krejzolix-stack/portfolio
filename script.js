9b83c1d6a39e3053992823e669414173ddfddeb8
krejzolix-stack <krejzolix@gmail.com>
2026-09-06 19:03:49 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+const button = document.getElementById("contactButton");
+const contactText = document.getElementById("contactText");
+
+button.addEventListener("click", function() {
+
+    contactText.textContent =
+        "Kontakt: github.com/krejzolix-stack";
+
+});
